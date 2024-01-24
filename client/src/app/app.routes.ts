7cec1b8ad9545d87ed36cd92704e498311a8448e
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-01-24 20:48:59 +0530

    changed pushed

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
+import { PopUpComponent } from './pop-up/pop-up.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'contact-us', component: ContactUsComponent },
     { path: 'home', component: HomeComponent },
     { path: 'services', component: ServicesComponent },
+    {path: 'pop-up', component: PopUpComponent},
     { path: '**', redirectTo: 'home' }
 ];
