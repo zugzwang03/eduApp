7181c2df4f16401f60bafab684c35a684f43ffbd
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-01-24 22:29:53 +0530

    updated pop-up for teacher and student login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { StudentService } from './student.service';
 import { studentRegistration } from './student';
+import { PopUpComponent } from './pop-up/pop-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, TitleBarComponent, HomeComponent, ServicesComponent, ContactUsComponent],
+  imports: [CommonModule, RouterLink, RouterOutlet, TitleBarComponent, HomeComponent, ServicesComponent, ContactUsComponent, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -26,13 +28,19 @@ export class AppComponent {
     address: "Student 1's address",
     password: "Student1@90"
   };
-  constructor(private studentService: StudentService) { }
+  constructor(private studentService: StudentService, private dialog: MatDialog) { }
   ngOnInit() {
     this.studentRegister();
   }
   async studentRegister() {
     await this.studentService.studentRegistration(this.student).subscribe(res => {
       console.log(JSON.parse(JSON.stringify(res)).message);
+    })
+  }
+  openDialogBox() {
+    let dialogRef = this.dialog.open(PopUpComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      
     })
   }
 
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
+import { StudentLoginComponent } from './student-login/student-login.component';
+import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'contact-us', component: ContactUsComponent },
     { path: 'home', component: HomeComponent },
     { path: 'services', component: ServicesComponent },
-    {path: 'pop-up', component: PopUpComponent},
+    { path: 'pop-up', component: PopUpComponent },
+    { path: 'student-login', component: StudentLoginComponent },
+    { path: 'teacher-login', component: TeacherLoginComponent },
     { path: '**', redirectTo: 'home' }
 ];
