8a8b661c9a95318512a0060a688be92597a3c349
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-01-24 21:23:40 +0530

    login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { AppComponent } from './app.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
+import { StudentLoginComponent } from './student-login/student-login.component';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'contact-us', component: ContactUsComponent },
     { path: 'home', component: HomeComponent },
     { path: 'services', component: ServicesComponent },
-    { path: '**', redirectTo: 'home' }
+    { path: 'student-login', component: StudentLoginComponent },
+    { path: 'student-sign-up', component: StudentRegistrationComponent },
+    { path: '**', redirectTo: 'home' },
 ];
