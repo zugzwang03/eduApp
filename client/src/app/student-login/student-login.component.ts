2005a85f9a8881b8086c444bfabe89580dd33090
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-02-12 09:45:54 +0530

    added login in backend

--- client/src/app/student-login/student-login.component.ts
+++ client/src/app/student-login/student-login.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StudentService } from '../student.service';
+import { StudentLogin } from '../student';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './student-login.component.html',
   styleUrl: './student-login.component.css'
 })
 export class StudentLoginComponent {
-
+  @Input() studentLogin: StudentLogin = {
+    email: '',
+    password: ''
+  }
+  constructor(private studentService: StudentService, private router: Router) { }
+  login() {
+    this.studentService.studentLogin(this.studentLogin).subscribe(res => {
+      if (JSON.parse(JSON.stringify(res)).student) {
+        console.log('login successful');
+        this.router.navigateByUrl('home');
+      } else {
+        console.log('login failed');
+        this.router.navigateByUrl('student-login');
+      }
+    });
+  }
 }
