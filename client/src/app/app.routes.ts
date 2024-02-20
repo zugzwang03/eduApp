bdcd5e7735cf73f2da80e8ca80c9329ac9accd54
nirman2004 <nd2719@ece.jgec.ac.in>
2024-02-20 14:55:25 +0530

    Adding registration popup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { PopUpComponent } from './pop-up/pop-up.component';
 import { StudentLoginComponent } from './student-login/student-login.component';
 import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 import { StudentRegistrationComponent } from './student-registration/student-registration.component';
+import { RegistrationPopUpComponent } from './registration-pop-up/registration-pop-up.component';
+import { TeacherRegistrationComponent } from './teacher-registration/teacher-registration.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -15,8 +17,9 @@ export const routes: Routes = [
     { path: 'services', component: ServicesComponent },
     { path: 'pop-up', component: PopUpComponent },
     { path: 'student-login', component: StudentLoginComponent },
-    { path: 'student-registration', component: StudentRegistrationComponent },
     { path: 'teacher-login', component: TeacherLoginComponent },
+    {path: 'registration_pop-up', component: RegistrationPopUpComponent},
+    {path: 'teacher_registration',component: TeacherRegistrationComponent},
     { path: 'student-registration', component: StudentRegistrationComponent },
     { path: '**', redirectTo: 'home' }
 ];
